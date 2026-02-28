d6ea04bfec80b749338867ec453552b86435e424
praveenamarie <jemipraveenamarie@gmail.com>
2026-02-28 13:57:57 +0530

    app.py created

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+
+# Database Configuration
+db_config = {
+    'host': 'book-rds.cinsoscgioqa.us-east-1.rds.amazonaws.com',
+    'user': 'admin',
+    'password': 'SuperSecretPass123',
+    'database': 'dev'
+}
